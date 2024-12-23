868c509a81cff2263e237d07371bb22d8c75dfa2
stowu2005 <stowu2005@gmail.com>
2024-12-22 19:02:15 -0800

    Added electoral college bar and highlights

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 let us
-let curr
+let curr = null
 let mapped_data
 let name_to_fips = new Map();
 var total_pop = 0
@@ -8,15 +8,23 @@ let named_counties = new Map()
 let counties_array
 let input
 let outline_map
-
+let states_map
+let prev_named_counties = []
+let state_totals = new Map()
+let state_population_gotten = new Map()
+let named_states = new Map()
+let state_electoral_votes
+let total_electoral_votes = 0
+let curr_state = null
 
 async function fetchJSONData() {
     try {
-        let [res, data] = await Promise.all([fetch("./data/counties-albers-10m.json"),
-            d3.csv("./data/uscounties.csv")]);
+        let [res, data, state_data] = await Promise.all([fetch("./data/counties-albers-10m.json"),
+            d3.csv("./data/uscounties.csv"), d3.csv("./data/votecount.csv")]);
         if (!res.ok) {
             throw new Error(`HTTP error! Status: ${res.status}`);
         }
+        state_electoral_votes = new Map(Array.from(state_data, d => [d.state, parseInt(d.num_votes)]))
         mapped_data = new Map(Array.from(data, d => [d.county_fips, (({ county, county_full, state_name, population }) => ({ county, county_full, state_name, population }))(d)]))
         data.forEach(d => {
             const key = d.county.toLowerCase().replace(/[^a-zA-Z]+/g, '');            
@@ -24,6 +32,12 @@ async function fetchJSONData() {
                 name_to_fips.set(key, [d.county_fips]);
             } else {
                 name_to_fips.get(key).push(d.county_fips)
+            }
+            if (!state_totals.has(d.state_name)) {
+                state_population_gotten.set(d.state_name, 0)
+                state_totals.set(d.state_name, parseInt(d.population))
+            } else {
+                state_totals.set(d.state_name, state_totals.get(d.state_name) + parseInt(d.population))
             }
         });
         us = await res.json();
@@ -37,15 +51,15 @@ async function fetchJSONData() {
 
         const svg = d3.select("#map")
             .attr("width", "72vw")
-            .attr("height", "60vw")
-            .attr('viewBox',`-100 0 ${width} ${height}`)
-            .attr('preserveAspectRatio', "xMidYMid meet");
+            .attr("height", "37vw")
+            .attr('viewBox',`-100 -190 ${width} ${height}`)
+            .attr('preserveAspectRatio', "xMidYMid slice");
 
         const path = d3.geoPath();
 
         const g = svg.append("g");
 
-        counties = g.append("g")
+        const counties = g.append("g")
             .attr("fill", "#444")
             .attr("stroke", "white")
             .attr("stroke-linejoin", "round")
@@ -65,14 +79,20 @@ async function fetchJSONData() {
 
         counties_array = counties._groups[0];
         
-        g.append("path")
+        const states = g.append("g")
             .attr("fill", "none")
             .attr("stroke", "white")
             .attr("stroke-width", 1.2)
             .attr("stroke-linejoin", "round")
-            .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.states).features)
+            .join("path")
+            .attr("d", path);
 
-        outline = g.append("g")
+        const states_array = states._groups[0]
+        states_map = new Map(states_array.map((d) => [d.__data__.properties.name, d]))
+
+        const outline = g.append("g")
             .attr("fill", "none")
             .attr("stroke", "blue")
             .attr("stroke-linejoin", "round")
@@ -82,13 +102,14 @@ async function fetchJSONData() {
         .data(topojson.feature(us, us.objects.counties).features)
         .join("path")
             .attr("d", path);
-        outline_array = outline._groups[0]
+        const outline_array = outline._groups[0]
 
         outline_map = new Map(outline_array.map((d) => [d.__data__.id, d]))
 
         barchart(0, 3142, "#bar-chart", "Number of counties guessed", "Number of counties remaining")
         barchart(0, 331092220, "#pop-chart", "Total population of counties guessed", "Population remaining")
-        
+        barchart(0, 538, "#vote-chart", "Electoral Votes Won", "Electoral Votes remaining")
+
         
         let tooltip = svg.append("g")
             .style("opacity", 0)
@@ -113,17 +134,19 @@ async function fetchJSONData() {
                 d3.zoomIdentity,
                 d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
             );
+            barchart(total_electoral_votes, 538, "#vote-chart", "Electoral Votes Won", "Electoral Votes remaining")
+            document.getElementById("total_votes").innerHTML = `The total electoral votes won is: ${total_electoral_votes} / 538`;
         }
 
         function clicked(event, d) {
             if (curr === d.id) {
                 curr = null
+                curr_state = null
                 reset();
                 return;
             } else {
                 curr = d.id
             }
-
             const [[x0, y0], [x1, y1]] = path.bounds(d);
             event.stopPropagation();
             outline.transition().attr("stroke-width", 0);
@@ -140,6 +163,12 @@ async function fetchJSONData() {
                 d3.pointer(event, svg.node())
             );
             d3.select(state_outline).raise();
+            curr_state = mapped_data.get(d.id).state_name
+            let state_gotten = state_population_gotten.get(curr_state)
+            let state_total = state_totals.get(curr_state)
+            barchart(state_gotten, state_total, "#vote-chart", "Total population of counties guessed in " + curr_state, "Population remaining in " + curr_state)
+            document.getElementById("total_votes").innerHTML = `The total population of counties named in ${curr_state} is: 
+                ${state_gotten.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")} / ${state_total.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")}`;
         }
 
         function zoomed(event) {
@@ -166,9 +195,16 @@ async function fetchJSONData() {
                     .attr("rx", 5 * squared_amt)
                     .attr("ry", 5 * squared_amt)
                     .attr("stroke-width", 1 + (squared_amt));
+                const fixedTranslation = 10 / zoom_dim.k;
+                console.log(zoom_dim)
                 tooltip
                     .style("opacity", 0.8)
-                    .attr("transform", `translate(${event.offsetX}, ${event.offsetY - 50})`);
+                    .attr("transform", `translate(${(event.offsetX - zoom_dim.x)/zoom_dim.k + fixedTranslation}
+                        , ${(event.offsetY - zoom_dim.y)/zoom_dim.k + fixedTranslation})`);
+                console.log(event.offsetX)
+                console.log(event.pageX)
+                console.log(event.offsetX + event.offsetX * 0.02 * (squared_amt - 2.8284271247461903))
+
                 tooltip_text
                     .attr("font-size", 8 * Math.sqrt(zoom_dim.k))
                     .append("tspan")
@@ -183,7 +219,7 @@ async function fetchJSONData() {
                     .text(`Population: ${mapped_data_county.population.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")}`)
                     .attr("y", box_height * 3/4)
                     .attr("x", box_width / 2)
-            }
+          }
         }
 
     } catch (error) {
@@ -204,20 +240,53 @@ function submit() {
     }
     fips = name_to_fips.get(input)
     d_list = counties_array.filter((d) => fips.includes(d.__data__.id))
-    for (const d of d_list) {
-        total_pop += parseInt(mapped_data.get(d.__data__.id).population)
-        total_counties += 1
+    for (const d of prev_named_counties) {
         d3.select(d).transition()
             .style("fill", "red");
+    }
+    prev_named_counties = d_list
+    let county_states = []
+    for (const d of d_list) {
+        d_county = mapped_data.get(d.__data__.id)
+        county_pop = parseInt(d_county.population)
+        county_states.push(d_county.state_name)
+        total_pop += county_pop
+        total_counties += 1
+        state_population_gotten.set(d_county.state_name, state_population_gotten.get(d_county.state_name) + county_pop)
+        d3.select(d).transition()
+            .style("fill", "#b6b004");
+    }
+    for (const state of county_states) {
+        if (!named_states.has(state)) {
+            if (state_population_gotten.get(state) * 2 >= state_totals.get(state)) {
+                named_states.set(state, 1)
+                total_electoral_votes += state_electoral_votes.get(state)
+                got_state = states_map.get(state)
+                d3.select(got_state).transition().style("stroke", "red");
+                d3.select(got_state).raise()
+
+            }
+        }
     }
     named_counties.set(input, d_list)
     barchart(total_counties, 3142, "#bar-chart", "Number of counties guessed", "Number of counties remaining")
     barchart(total_pop, 331092220, "#pop-chart", "Total population of counties guessed", "Population remaining")
 
-
     document.getElementById("counties_named").innerHTML = total_counties.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     document.getElementById("total_population").innerHTML = total_pop.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     document.getElementById("input_bar").value = "";
+    if (curr !== null) {
+        let state_gotten = state_population_gotten.get(curr_state)
+        let state_total = state_totals.get(curr_state)
+        barchart(state_gotten, state_total, "#vote-chart", "Total population of counties guessed in " + curr_state, "Population remaining in " + curr_state)
+        document.getElementById("total_votes").innerHTML = `The total population of counties named in ${curr_state} is: 
+            ${state_gotten.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")} / ${state_total.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")}`;
+    } else {
+        barchart(total_electoral_votes, 538, "#vote-chart", "Electoral Votes Won", "Electoral Votes remaining")
+        document.getElementById("total_votes").innerHTML = `The total electoral votes won is: ${total_electoral_votes} / 538`;
+    }
+
+
 }
 
 fetchJSONData()
